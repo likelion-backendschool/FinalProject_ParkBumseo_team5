c9548812e5fc98dcdc53d4f8fc2414d88986d8a7
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-11-02 14:57:23 +0900

    refactor : 권한 수정

--- src/main/java/com/ll/exam/final__2022_10_08/app/admin/controller/AdminController.java
+++ src/main/java/com/ll/exam/final__2022_10_08/app/admin/controller/AdminController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
     @GetMapping("/home/main")
-    @PreAuthorize("isAuthenticated()")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String homePage(){
 
         return "adm/home/main";
